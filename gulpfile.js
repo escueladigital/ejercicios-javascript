b69aea2e38098d2b599ffc33f31a3dba8a71ec1d
Escuela Digital <ceo@escueladigital.pe>
2016-02-27 08:21:07 -0500

    creadas las tareas es6 y sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,48 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const babel = require('gulp-babel');
-const path = require('path');
-const autoprefixer = require('gulp-autoprefixer');
+   const gulp = require('gulp'),
+         sass = require('gulp-sass'),
+        babel = require('gulp-babel'),
+         path = require('path'),
+ autoprefixer = require('gulp-autoprefixer'),
+       rename = require('gulp-rename');
+
+
+// gulp.task('sass', () =>
+//   gulp.src('./**/css/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions'],
+//       cascade: true
+//     }))
+//     .pipe(gulp.dest(path.dirname()))
+// );
 
 
 gulp.task('sass', () =>
-  gulp.src('./**/css/*.scss')
+  gulp.src('./**/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: true
     }))
-    .pipe(gulp.dest(path.dirname()))
+    .pipe(rename( (currentPath) =>
+      currentPath.dirname = path.join(currentPath.dirname, '..', 'css')
+    ))
+    .pipe(gulp.dest('./'))
 );
 
+gulp.task('es6',()=>
+  gulp.src('./**/es6/*.js')
+      .pipe(babel({
+        presets: ['es2015']
+      }))
+      .pipe(rename( (currentPath) =>
+        currentPath.dirname = path.join(currentPath.dirname, '..', 'js')
+      ))
+      .pipe(gulp.dest('./'))
+);
 
-gulp.task('default', ()=>
- gulp.watch('./**/css/*.scss', ['sass'])
+gulp.task('default', ()=> {
+    gulp.watch('./**/css/*.scss', ['sass']);
+    gulp.watch('./**/es6/*.js', ['es6']);
+  }
 );
