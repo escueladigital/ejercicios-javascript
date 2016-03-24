592001807114d3fc010803f2913f2bfbe7a758b9
Escuela Digital <ceo@escueladigital.pe>
2016-03-24 18:48:11 -0500

    ejercicio 01 - menu responsive funcionando hay que refactorizar codigo y anadir evento resize

--- 01-responsive-menu/dev/js/scripts.js
+++ 01-responsive-menu/dev/js/scripts.js
@@ -1,3 +1,45 @@
-const yo = "Alvaro Felipe Chavez";
-var title = document.querySelector('h1');
-title.textContent = "He cambiado nuevamente";
+// check if a menu__item contains a child menu
+
+var menu = document.getElementById('menu');
+var menuItems = menu.children;
+var breakpoint = 1000;
+
+// check if a menu__item contains a child menu
+for(let i = 0; i < menuItems.length; i++) {
+  let currentItem = menuItems[i];
+  if(currentItem.querySelector('ul')){
+    currentItem.classList.add('menu__parent');
+  }
+}
+var menuParents = menu.querySelectorAll('.menu__parent');
+
+// Show and hide child menu in hover parent
+if(innerWidth > breakpoint) {
+  menu.classList.add('horizontal');
+  for (let i = 0; i < menuParents.length; i++) {
+    let currentItem = menuParents[i];
+    let menuChild = menuParents[i].querySelector('.menu--child');
+    currentItem.addEventListener('mouseenter', () => {
+      menuChild.classList.add('show');
+    });
+    currentItem.addEventListener('mouseleave', () => {
+      menuChild.classList.remove('show');
+    })
+  }
+} else {
+  for (let i = 0; i < menuParents.length; i++) {
+    let currentItem = menuParents[i];
+    let menuChild = menuParents[i].querySelector('.menu--child');
+    let contador = 0;
+    currentItem.addEventListener('click', () => {
+      if(contador === 0){
+        menuChild.classList.add('show');
+        contador++;
+      } else if(contador === 1){
+        menuChild.classList.remove('show');
+        contador--;
+      }
+      console.log(contador);
+    });
+  }
+}
--- 01-responsive-menu/dist/js/scripts.js
+++ 01-responsive-menu/dist/js/scripts.js
@@ -1,5 +1,56 @@
-"use strict";
+'use strict';
 
-var yo = "Alvaro Felipe Chavez";
-var title = document.querySelector('h1');
-title.textContent = "He cambiado nuevamente";
+// check if a menu__item contains a child menu
+
+var menu = document.getElementById('menu');
+var menuItems = menu.children;
+var breakpoint = 1000;
+
+// check if a menu__item contains a child menu
+for (var i = 0; i < menuItems.length; i++) {
+  var currentItem = menuItems[i];
+  if (currentItem.querySelector('ul')) {
+    currentItem.classList.add('menu__parent');
+  }
+}
+var menuParents = menu.querySelectorAll('.menu__parent');
+
+// Show and hide child menu in hover parent
+if (innerWidth > breakpoint) {
+  menu.classList.add('horizontal');
+
+  var _loop = function _loop(i) {
+    var currentItem = menuParents[i];
+    var menuChild = menuParents[i].querySelector('.menu--child');
+    currentItem.addEventListener('mouseenter', function () {
+      menuChild.classList.add('show');
+    });
+    currentItem.addEventListener('mouseleave', function () {
+      menuChild.classList.remove('show');
+    });
+  };
+
+  for (var i = 0; i < menuParents.length; i++) {
+    _loop(i);
+  }
+} else {
+  var _loop2 = function _loop2(i) {
+    var currentItem = menuParents[i];
+    var menuChild = menuParents[i].querySelector('.menu--child');
+    var contador = 0;
+    currentItem.addEventListener('click', function () {
+      if (contador === 0) {
+        menuChild.classList.add('show');
+        contador++;
+      } else if (contador === 1) {
+        menuChild.classList.remove('show');
+        contador--;
+      }
+      console.log(contador);
+    });
+  };
+
+  for (var i = 0; i < menuParents.length; i++) {
+    _loop2(i);
+  }
+}
